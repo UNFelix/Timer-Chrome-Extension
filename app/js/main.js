2ba851ea312bf85f3b986535a73c50f6dee81471
unfelix <shevko.evheniy@gmail.com>
2021-12-31 16:20:28 +0200

    activities dropdown + animation

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,22 @@
-const pauseButtons = document.querySelectorAll(".pause__task");
-const playButtons = document.querySelectorAll(".activate__task");
+const pauseBtns = document.querySelectorAll(".pause__task");
+const playBtns = document.querySelectorAll(".activate__task");
+const activitiesBtn = document.querySelector('.subheader__tasks');
+const activitiesDropDown = document.querySelector('.activities');
 
-pauseButtons.forEach(function (btn) {
+pauseBtns.forEach(function (btn) {
   btn.addEventListener("click", function () {
     this.previousElementSibling.classList.remove("hidden");
     this.classList.add("hidden");
   });
 });
 
-playButtons.forEach(function (btn) {
+playBtns.forEach(function (btn) {
   btn.addEventListener("click", function () {
     this.nextElementSibling.classList.remove("hidden");
     this.classList.add("hidden");
   });
 });
+
+activitiesBtn.addEventListener('click', function () {
+  activitiesDropDown.classList.toggle('hidden');
+})
