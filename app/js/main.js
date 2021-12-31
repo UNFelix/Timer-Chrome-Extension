bd15c36424aa93ae0adb1a72c4a265c197058188
unfelix <shevko.evheniy@gmail.com>
2021-12-31 12:57:24 +0200

    activate__task & pause__task toggling

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,32 @@
-const pauseTaskBtn = document.querySelector('.pause__task');
-const activateTaskBtn = document.querySelector('.activate__task');
+// const pauseTaskBtn = document.querySelector('.pause__task');
+// const activateTaskBtn = document.querySelector('.activate__task');
 
-activateTaskBtn.addEventListener('click', function () {
-  pauseTaskBtn.classList.remove('hidden')
-  activateTaskBtn.classList.add('hidden')
-})
-pauseTaskBtn.addEventListener('click', function () {
-  activateTaskBtn.classList.remove('hidden')
-  pauseTaskBtn.classList.add('hidden')
-})
+
+
+// activateTaskBtn.addEventListener('click', function () {
+//   pauseTaskBtn.classList.remove('hidden');
+//   activateTaskBtn.classList.add('hidden');
+// })
+
+
+// pauseTaskBtn.addEventListener('click', function () {
+//   activateTaskBtn.classList.remove('hidden');
+//   pauseTaskBtn.classList.add('hidden');
+// })
+
+const pauseButtons = document.querySelectorAll(".pause__task");
+const playButtons = document.querySelectorAll(".activate__task");
+
+pauseButtons.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    this.previousElementSibling.classList.remove("hidden");
+    this.classList.add("hidden");
+  });
+});
+
+playButtons.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    this.nextElementSibling.classList.remove("hidden");
+    this.classList.add("hidden");
+  });
+});
