2a0e0292c03dc32700f00f57aeff64a89e039ced
unfelix <shevko.evheniy@gmail.com>
2022-01-12 08:01:03 +0200

    project select dropdown

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,8 @@ const pauseBtns = document.querySelectorAll(".pause__task");
 const playBtns = document.querySelectorAll(".activate__task");
 const activitiesBtn = document.querySelector('.subheader__tasks');
 const activitiesDropDown = document.querySelector('.activities');
+const projectsBtn = document.querySelector('.header__projects');
+const projectsDropDown = document.querySelector('.projects');
 
 pauseBtns.forEach(function (btn) {
   btn.addEventListener("click", function () {
@@ -19,4 +21,8 @@ playBtns.forEach(function (btn) {
 
 activitiesBtn.addEventListener('click', function () {
   activitiesDropDown.classList.toggle('hidden');
+})
+
+projectsBtn.addEventListener('click', function () {
+  projectsDropDown.classList.toggle('hidden');
 })
